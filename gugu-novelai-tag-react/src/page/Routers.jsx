d23dafdced5812d7b6bdd81c8e5a562f24501ead
MinMin <MinMin@gugu.dev>
2024-02-22 12:46:47 +0800

    demo分支用于部署项目站点

--- gugu-novelai-tag-react/src/page/Routers.jsx
+++
@@ -1,9 +0,0 @@
-import {Component} from "react";
-import {renderRoutes} from "react-router-config";
-import routers from "../config/router";
-
-export default class Routers extends Component {
-    render() {
-        return renderRoutes(routers);
-    }
-}
